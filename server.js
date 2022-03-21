6d14481bb7f54a77fa69aabc9ee90e022055af96
Brayan-Bertrand-Hernandez <almibar2@gmail.com>
2022-03-20 21:19:39 -0600

    Change port and adding a url prefix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, world!'))
+app.get('/myWebsite', (req, res) => res.send('Hello, world!'))
 
-app.listen(3000);
-console.log('Server listening on port 3000')
+app.listen(8080);
+console.log('Server on port 8080')
