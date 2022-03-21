96c3d180f44fa39af51694b80119fecb779061cc
Brayan-Bertrand-Hernandez <almibar2@gmail.com>
2022-03-20 21:36:08 -0600

    Change redirects

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.redirect('myWebsite')
+})
+
 app.get('/myWebsite', (req, res) => res.send('Hello, world!'))
 
-app.listen(8080);
-console.log('Server on port 8080')
+app.listen(PORT, function(err){
+    if (err) console.log(err)
+    
+    console.log("Server listening on PORT", PORT)
+})
