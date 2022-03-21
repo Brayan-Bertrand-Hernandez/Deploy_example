7016ce3ca6465b8ee66fc7d8f6d2a39741bdfaf6
Brayan-Bertrand-Hernandez <almibar2@gmail.com>
2022-03-20 22:08:34 -0600

    Adding router module and path module use case

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const router = express.Router();
 
 const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.redirect('myWebsite')
-})
+router.get('/', (req, res) => {
+    res.redirect('myWebsite/index.html');
+});
 
-app.get('/myWebsite', (req, res) => res.send('Hello, world!'))
+router.get('/myWebsite', (req, res) => {
+    res.redirect('myWebsite/index.html');
+});
+
+router.get('/myWebsite/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+
+app.use('/', router);
 
 app.listen(PORT, function(err){
-    if (err) console.log(err)
-    
-    console.log("Server listening on PORT", PORT)
-})
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
